e504ac3e48fe018917da043beef96f1f9921f3e1
Marcus Dale Rundberg <marcus.dale.rundberg@gmail.com>
2014-10-10 09:03:16 +0200

    INIT

---
+++ testProject/Assets/goToNextScene.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class goToNextScene : MonoBehaviour {
+
+	void Awake () {
+		Application.targetFrameRate = -1;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetMouseButtonDown(0)){
+			var ray = Camera.main.ScreenPointToRay (Input.mousePosition); 
+			
+			var origin = new Vector2 (ray.origin.x, ray.origin.y); 
+			
+			var hit = Physics2D.Raycast (origin, new Vector2 (0, 0));	
+			
+			if (hit) {
+				if(hit.transform.name == "Cube") {
+
+					Application.LoadLevel ("scenetwo");
+				}
+			} 
+	
+		}
+	}
+}
---
+++ testProject/Assets/moveCamera.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveCamera : MonoBehaviour {
+	
+	void Awake () {
+		Application.targetFrameRate = -1;
+	}
+	
+	// Use this for initialization
+	void Start () {
+		
+		rigidbody2D.velocity = new Vector2(8, 0);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
